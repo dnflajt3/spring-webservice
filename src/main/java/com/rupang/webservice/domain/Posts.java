bda3c11369b366d209df683abf5762da6614e59e
dnflajt3 <dnflajt3@naver.com>
2021-10-28 09:46:49 +0900

    푸쉬테스트

---
+++ src/main/java/com/rupang/webservice/domain/Posts.java
@@ -0,0 +1,36 @@
+package com.rupang.webservice.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+public class Posts {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+	
+	@Column(length = 500,nullable = false)
+	private String title;
+	
+	@Column(columnDefinition = "Text",nullable = false)
+	private String content;
+	
+	private String author;
+	
+	@Builder
+	public Posts(String title,String content,String author) {
+		this.author=title;
+		this.content=content;
+		this.author=author;
+	}
+}
