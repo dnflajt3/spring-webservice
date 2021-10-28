33402ecc61003e12243133b17c411c5af869009d
dnflajt3 <dnflajt3@naver.com>
2021-10-28 09:10:33 +0900

    모든 변경 내역은 Commit 단위로 관리된다. Push는 원격저장소에 Commit된 내용을 반영하는것이다.

--- src/main/java/com/rupang/webservice/web/WebRestController.java
+++ src/main/java/com/rupang/webservice/web/WebRestController.java
@@ -6,6 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class WebRestController {
 	@GetMapping("/hello")
     public String hello() {
-        return "HelloWorld";
+        return "Hey";
     }
 }
